b2528b6d18bd70b46f1ffcd9bc45914d16cbea21
Phil Zona <phil.zona@vercel.com>
2025-02-10 02:19:32 -0500

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+# Make sure this includes GITHUB_TOKEN to avoid rate limiting
+source "$HOME/.privaterc"
+
 # Set up some important directories if not already created
 mkdir -p "$HOME/.local/{bin,share,state}"
 
 # Check for zsh
-USING_ZSH=$(test [[ $(echo $SHELL | tr '/' '\n' | tail -n 1) == 'zsh' ]])
-if [[ ! $USING_ZSH ]]; then
+if [[ ! "$(echo $SHELL | tr '/' '\n' | tail -n 1)" == "zsh" ]]; then
   echo "Please set up zsh and try again"
   exit 1
 fi
@@ -19,10 +21,14 @@ fi
 # but need to test 
 #
 
-curl "https://mise.run" | sh
-command -v mise && \
-  mise activate zsh && \
-  mise install
+if [[ ! -f "$HOME/.local/bin/mise" ]]; then
+  curl "https://mise.run" | sh
+  command -v mise && \
+    mise activate zsh && \
+    mise install
+else
+  echo "Mise is already installed, skipping..."
+fi
 
 # Install a local server that will help grab binaries 
 # that I don't want to manage with mise of system package manager
@@ -32,14 +38,14 @@ command -v mise && \
 
 BIN_DIR="$HOME/.local/bin"
 (cd "$BIN_DIR" && source "$DOTFILES/scripts/installer.sh")
-installer # Starts the server
+installer & # Starts the server as a bg process
 
 # Grab a few key packages, will move a bunch more over as I test this more
 #
 
 # neovim
-(cd "$BIN_DIR && curl -s http://localhost:3000/neovim/neovim?as=nvim | bash)
+(cd "$BIN_DIR" && curl -s "http://localhost:3000/neovim/neovim?as=nvim" | bash)
 # fzf
-(cd "$BIN_DIR && curl -s http://localhost:3000/junegunn/fzf | bash)
+(cd "$BIN_DIR" && curl -s "http://localhost:3000/junegunn/fzf" | bash)
 # zoxide
-(cd "$BIN_DIR && curl -s http://localhost:3000/ajeetdsouza/zoxide | bash)
+(cd "$BIN_DIR" && curl -s "http://localhost:3000/ajeetdsouza/zoxide" | bash)

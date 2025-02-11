21a826e8df53b9d85df264902ed6d459e435ae38
Phil Zona <phil.b.zona@gmail.com>
2025-02-10 23:18:45 -0500

    nvim settingss

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,7 +1,25 @@
 require "nvchad.options"
 
--- add yours here!
-
 local o = vim.o
-o.relativenumber = true
--- o.cursorlineopt ='both' -- to enable cursorline!
+local opt = vim.opt
+
+o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Line numbers
+opt.nu = true
+opt.rnu = true
+
+-- Enable mouse mode
+opt.mouse = "a"
+
+-- Allow access to system clipboard
+opt.clipboard = "unnamed,unnamedplus"
+
+-- Always keep 8 lines above/below cursor unless at start/end of file
+opt.scrolloff = 8
+
+-- Place a column line
+opt.colorcolumn = "80"
+
+-- Always show the sign column
+opt.signcolumn = "yes"

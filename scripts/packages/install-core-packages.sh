689a420b75b760ba572b02dcbef3294069373ea4
Phil Zona <phil.b.zona@gmail.com>
2025-04-09 11:10:16 -0400

    add fd-find for nvim explorer

--- scripts/packages/install-core-packages.sh
+++ scripts/packages/install-core-packages.sh
@@ -4,33 +4,33 @@
 OS=$(detect_os)
 
 case "$OS" in
-  "linux")
-    sudo apt install \
-      tmux \
-      docker.io \
-      openssl \
-      fail2ban
+"linux")
+  sudo apt install \
+    tmux \
+    docker.io \
+    openssl \
+    fail2ban \
+    fd-find # Needed for initial nvim exlorer config, doens't pick up mise for some reason
   ;;
-  "macos") 
-    brew install \
-      tmux \
-      docker \
-      openssl \
-    
-    # Wezterm (terminal emulator)
-    # https://wezterm.org/
-    #
-    brew install --cask wezterm
+"macos")
+  brew install \
+    tmux \
+    docker \
+    openssl \
+    fd
 
-    # Aerospace (tiling window manager)
-    # https://github.com/nikitabobko/Aerospace
-    #
-    brew install --cask nikitabobko/tap/aerospace
-    defaults write -g NSWindowShouldDragOnGesture -bool true
+  # Wezterm (terminal emulator)
+  # https://wezterm.org/
+  #
+  brew install --cask wezterm
+
+  # Aerospace (tiling window manager)
+  # https://github.com/nikitabobko/Aerospace
+  #
+  brew install --cask nikitabobko/tap/aerospace
+  defaults write -g NSWindowShouldDragOnGesture -bool true
   ;;
-  *) echo default
+*)
+  echo default
   ;;
 esac
-
-
-

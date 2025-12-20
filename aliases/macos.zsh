aa17fc22649309a912662b2dc77000c9f329ada7
Phil Zona <phil.zona@vercel.com>
2025-12-20 13:20:45 -0500

    Add debug config file alias

--- aliases/macos.zsh
+++ aliases/macos.zsh
@@ -3,3 +3,7 @@ alias cat="bat"
 
 alias wd="pwd | pbcopy"
 alias p="pbpaste"
+
+# Create a new debug config for Next.js projects
+alias mkdebug="[ -f .vscode/launch.json ] && mv .vscode/launch.json .vscode/launch.json.bak; mkdir -p .vscode && curl -s https://gist.githubusercontent.com/pbzona/026afabbb28a5acd013e0f1b95936d2f/raw > .vscode/launch.json"
+

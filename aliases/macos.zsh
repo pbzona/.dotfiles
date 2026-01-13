53269607bc01df99356b25673ae0127bfef254e3
Phil Zona <phil.zona@vercel.com>
2026-01-13 11:16:55 -0500

    Add dangerous claude alias

--- aliases/macos.zsh
+++ aliases/macos.zsh
@@ -7,3 +7,6 @@ alias p="pbpaste"
 # Create a new debug config for Next.js projects
 alias mkdebug="[ -f .vscode/launch.json ] && mv .vscode/launch.json .vscode/launch.json.bak; mkdir -p .vscode && curl -s https://gist.githubusercontent.com/pbzona/026afabbb28a5acd013e0f1b95936d2f/raw > .vscode/launch.json"
 
+# Start a dangerous claude session for looping
+alias ccd='claude --dangerously-skip-permissions'
+
